a02d1bdb214085974afc267fa294b0e6f3df87ba
NeethuJosephh <neethu1212001@gmail.com>
2023-02-18 17:35:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { FooterComponent } from './units/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './pages/form/form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    FormComponent,
+    
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { HeroService } from 'src/app/hero.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  constructor(private hero:HeroService){
+
+  }
+  todo:any
+  ngOnInit(){
+this.hero.getData().subscribe(data=>
+{
+  this.todo=data;
+})
+
+  console.log(this.todo);
+  }
+  updateitem(id:any){
+    
+
+  }
+  deleteitem(id:any){
+    this.todo=this.todo.filter((t:any)=>t.id !=id)
+
+  }
+
+}
